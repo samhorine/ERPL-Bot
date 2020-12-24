0dffba7d7300a709cb6c76e0844c8ab57dcd911a
newcomb-luke <newcomb.luke@gmail.com>
2020-12-24 00:26:14 -0500

    Added member role addition

--- erplbot/__main__.py
+++ erplbot/__main__.py
@@ -1,6 +1,120 @@
 
+import discord
+from erplbot.club_members import get_members_from_spreadsheet, Name
+from erplbot.sheets import GoogleSheets, retrieve_credentials
+
+# NOTE: Use this link to add ERPL Bot to the ERPL Discord
+# https://discord.com/api/oauth2/authorize?client_id=791401092977786883&permissions=8&scope=bot
+
+# The bot's Discord Bot token
+BOT_TOKEN = 'NzkxNDAxMDkyOTc3Nzg2ODgz.X-OntA.iZ6F4sS1ndt6NEACFmgBd3rJUM8'
+
+# The ID of the spreadsheet that contains all of ERPL's current members
+SPREADSHEET_ID = '1v462rOLQhDgFU4bg8VN7LgauLgE1r8kI_jiAPWzq6Eg'
+
+# These specify when the member's data in the spreadsheet starts and ends
+RANGE_START = 'A2'
+RANGE_END = 'I'
+
+# This is Discord's ID that means the "Member" role
+MEMBER_ROLE_ID = 791509013401567244
+RECRUIT_ROLE_ID = 791534387900448789
+
+# This variable will store our GoogleSheets instance
+google_sheets = None
+# This one will store our Google API credentials
+creds = None
+
+class ERPLBot(discord.Client):
+    """
+    This class represents the core functionality of the ERPL Discord Bot
+    """
+    async def on_ready(self):
+        """
+        This function runs when the bot is connected to Discord
+        """
+        global google_sheets
+
+        # Let's connect to Google's API now
+        google_sheets = GoogleSheets(creds)
+
+        print("Bot initialized")
+    
+    async def on_member_join(self, member):
+        """
+        This function runs whenever a new member joins the server
+        """
+        # Here we will just call the update_members function
+        await self.update_members(member.guild)
+
+    async def on_member_update(self, before, after):
+        """
+        This function runs whenever a new member updates their own profile, like changing their nickname
+        """
+        # Here we will just call the update_members function
+        await self.update_members(before.guild)
+    
+    async def update_members(self, guild):
+        """
+        Updates all members in the ERPL Discord by checking their names, roles, and the spreadsheet
+        """
+        # "Guild" is the internal name for servers. This gets all members currently in the server
+        discord_members = await guild.fetch_members().flatten()
+
+        # Retrieves all current ERPL members listed in the spreadsheet as ClubMember instances
+        spreadsheet_members = get_members_from_spreadsheet(google_sheets, SPREADSHEET_ID, ':'.join([RANGE_START, RANGE_END]))
+
+        # Loop through each member in the Discord
+        for discord_member in discord_members:
+            # Let's check if it is even worth our time to check if they are in the spreadsheet
+            # We will check if they already have the member role
+            if MEMBER_ROLE_ID in list(map(lambda role: role.id, discord_member.roles)):
+                # Then just skip over them
+                continue
+            
+            # If they don't have the role, we need to check if they are in the spreadsheet
+            # First though, we need to get their name
+            name = None
+
+            # If this member has no nickname
+            if discord_member.nick is None:
+                name = Name.from_str(discord_member.name)
+            # If they do have a nickname
+            else:
+                name = Name.from_str(discord_member.nick)
+
+            # Iterate through each member in the spreadsheet
+            for member in spreadsheet_members:
+                # Check if their name is in the spreadsheet
+                if name == member.name:
+                    # If it is, then we need to add the member role
+                    member_role = guild.get_role(MEMBER_ROLE_ID)
+                    await discord_member.add_roles(member_role, reason='Found user in club spreadsheet')
+                    # We also need to remove the recruit role
+                    recruit_role = guild.get_role(RECRUIT_ROLE_ID)
+                    await discord_member.remove_roles(recruit_role)
+
+                    print(f'Added member role to {name}')
+
+                    # We also need to make sure they are marked as added in the spreadsheet
+                    member.update_in_server(google_sheets, SPREADSHEET_ID, RANGE_END, True)
+
 def main():
-    print("Hello world")
+    """
+    Our "main" function
+    """
+    global creds
+
+    # Reads our Google API credentials before starting the bot
+    creds = retrieve_credentials()
+
+    # Sets up our intents as a Discord Bot
+    intents = discord.Intents.default()
+    intents.members = True
+
+    # Connects to Discord and runs our bot with the bot's token
+    client = ERPLBot(intents=intents)
+    client.run(BOT_TOKEN)
 
 if __name__ == "__main__":
     main()
---
+++ erplbot/sheets.py
@@ -0,0 +1,82 @@
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+
+# If modifying these scopes, delete the AUTH_FILE
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
+
+AUTH_FILE = 'auth.bin'
+SECRETS_FILE = 'secrets.json'
+
+class GoogleSheets:
+    """
+    Represents a small custom interface to the Google Sheets API
+    """
+    def __init__(self, creds):
+        """
+        Creates a new GoogleSheets instance with the supplied credentials
+        """
+        self.creds = creds
+        # Creates a new Google API request service
+        service = build('sheets', 'v4', credentials=self.creds)
+        self.sheets = service.spreadsheets()
+    
+    def get_values(self, sheetId, value_range):
+        """
+        Sends a request to Google's API to retrieve data from the given range, from the spreadsheet with the given id
+        """
+        # Sets up the API request
+        request = self.sheets.values().get(spreadsheetId=sheetId, range=value_range)
+        # Executes the API request
+        result = request.execute()
+        # Extracts the values from the result
+        values = result.get('values', [])
+
+        return values
+    
+    def set_values(self, sheetId, value_range, new_values):
+        """
+        Sends a request to Google's API to set the data in the given range
+        """
+        # Sets up the body of the request we are going to send
+        body = { 'values': new_values }
+        # Sets up the API request
+        request = self.sheets.values().update(spreadsheetId=sheetId,
+                                              range=value_range,
+                                              valueInputOption='RAW',
+                                              body=body)
+        # Executes the API request
+        _result = request.execute()
+        
+
+def retrieve_credentials():
+    """
+    Retrieves credentials either from the AUTH_FILE or prompts the user for new credentials
+    """
+
+    credentials = None
+
+    # If the authentication credentials exist, then read them from the file
+    if os.path.exists(AUTH_FILE):
+        with open(AUTH_FILE, 'rb') as f:
+            credentials = pickle.load(f)
+    
+    # If the credentials were not found or are invalid
+    if not credentials or not credentials.valid:
+        # If they can be refreshed
+        if credentials and credentials.expired and credentials.refresh_token:
+            # Refresh them
+            credentials.refresh(Request())
+        else:
+            # Attempt to create new credentials with the provided scopes
+            flow = InstalledAppFlow.from_client_secrets_file(SECRETS_FILE, SCOPES)
+            # This prompts the user to login to their Google account and authorize this application
+            credentials = flow.run_local_server(port=0)
+        
+        # No matter what, the credentials have been updated, so write them
+        with open(AUTH_FILE, 'wb') as f:
+            pickle.dump(credentials, f)
+        
+    return credentials
