2c05257d10b5c934b9a00dab3082747dac38ccf5
ice-fly <ice_fly.alex@yahoo.com>
2021-01-23 17:04:10 -0500

    Add make config
    Moved sensitive data to pickled bin (not shown)

---
+++ makeConfig.py
@@ -0,0 +1,20 @@
+import pickle
+# The bot's Discord Bot token
+BOT_TOKEN = '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
+
+# The ID of the spreadsheet that contains all of ERPL's current members
+SPREADSHEET_ID = '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
+
+# These specify when the member's data in the spreadsheet starts and ends
+SHEET_NAME = 'SheetName!'
+RANGE_START = 'A2'
+RANGE_END = 'D'
+
+# This is Discord's ID that means the "Member" role
+MEMBER_ROLE_ID = 000000000000000000
+RECRUIT_ROLE_ID = 000000000000000000
+
+#Use Pickle to write config.bin
+with open('config.bin', 'wb') as fh:
+    pickle.dump([BOT_TOKEN, SPREADSHEET_ID, SHEET_NAME, RANGE_START, RANGE_END, MEMBER_ROLE_ID, RECRUIT_ROLE_ID], fh)
+exit
